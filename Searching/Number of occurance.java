7cc6de5b0c4bae29cb79d40dc7da9e97e5625bf3
Pandi Hema Gangothri <hemagangothri34@gmail.com>
2026-01-02 10:37:24 +0530

    Add files via upload

---
+++ Searching/Number of occurance.java
@@ -0,0 +1,46 @@
+class Solution {
+    
+        public static int lowerBound(int[]  arr,int target){
+            int res = arr.length;
+            
+            int lo = 0,hi = arr.length - 1;
+            
+            while(lo <= hi){
+                int mid  = lo +(hi - lo)/2;
+                if(arr[mid] >= target) {
+                    res = mid;
+                    hi = mid - 1;
+                    
+                }else {
+                    lo = mid + 1;
+                }
+            }
+            return res;
+        }
+    public static int upperBound(int[] arr,int target){
+        int res = arr.length;
+        int lo = 0, hi = arr.length -1;
+        while(lo <= hi){
+            
+            int mid = lo+(hi - lo)/2;
+            if(arr[mid] > target){
+                res = mid;
+                hi = mid - 1;
+                
+            }else{
+                lo = mid + 1;
+            }
+        }
+        return res;
+    
+        
+    }
+    public static int countFreq(int[] arr,int target){
+        return upperBound(arr,target) - lowerBound(arr,target);
+    }
+    public static void mian(String[] args){
+        int[] arr= {1,1,2,2,2,2,3};
+        int target = 2;
+        System.out.println(countFreq(arr, target));
+    }
+}
